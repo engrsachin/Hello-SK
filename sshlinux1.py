8bc74d0b865c39682d2bfcfccae85b85eaedd54e
engrsachin <33897817+engrsachin@users.noreply.github.com>
2018-05-15 21:46:38 +0530

    Add files via upload

---
+++ sshlinux1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import paramiko
+
+def sshCommand(hostname, port, username, password, command):
+    sshClient = paramiko.SSHClient()
+    sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    sshClient.load_system_host_keys()
+    sshClient.connect(hostname, port, username, password)
+    stdin, stdout, stderr = sshClient.exec_command(command)
+    print (stdout.read())
+
+if __name__=='__main__':
+    sshCommand('172.30.202.129', 22, 'lsintr1', 'lsIntr99!', 'uname -a')
