f104bceeed70915be19b4ec2d50c3f4ac67fd875
engrsachin <33897817+engrsachin@users.noreply.github.com>
2018-06-01 22:41:34 +0530

    Add files via upload

---
+++ Wloop_ReverseNum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#Python Program to Reverse a Number Using While Loop
+
+Num = int(input("Please Enter any Number: "))
+Reverse = 0
+while(Num > 0):
+    Reminder = Num %10
+    Reverse = (Reverse *10) + Reminder
+    Num = Num //10
+
+print("\n Reverse of entered number is = %d" %Reverse)
